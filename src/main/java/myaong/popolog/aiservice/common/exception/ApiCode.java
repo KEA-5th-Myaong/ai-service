37203eb38aed826f9b18336af01fa72e1ffaa990
chogh824 <chogh824@naver.com>
2024-12-13 03:14:16 +0900

    ⚙️Setting: 예외 처리

---
+++ src/main/java/myaong/popolog/aiservice/common/exception/ApiCode.java
@@ -0,0 +1,23 @@
+    package myaong.popolog.aiservice.common.exception;
+
+    import lombok.Getter;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.http.HttpStatus;
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum ApiCode {
+
+        OK(HttpStatus.OK, "COMMON_2000", "OK"),
+        INVALID_DATA(HttpStatus.BAD_REQUEST, "COMMON_4000", "Request data missing or invalid"),
+        READ_ONLY_ACCESS(HttpStatus.FORBIDDEN, "COMMON_4031", "You can only read"),
+        METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "COMMON_4050", "Method not allowed"),
+        INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_5000", "Internal Server Error"),
+        DB_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_5001", "DB Error"),
+        OBJECT_STORAGE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_5002", "Object Storage Error");
+
+        private final HttpStatus httpStatus;
+        private final String code;
+        private final String message;
+    }
+
